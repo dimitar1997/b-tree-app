990de6c702bb36a7176122d6792cffc4dbf98b67
Dimitar Chakarov <dchakarov@astea.net>
2024-11-01 11:20:37 +0200

    testing methods for remove

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useLayoutEffect, useState } from 'react';
 import './App.css';
 import { BTree, BTreeNode } from './B-Tree-Logic/B-Tree-Node';
 
@@ -7,6 +7,38 @@ function App() {
   const [gap, setGap] = useState(0.2);
   const [value, setValue] = useState<string>('');
   const [btree, setBtree] = useState(new BTree<number>())
+
+
+  useLayoutEffect(() => {
+    // Create a new BTree instance
+    // const newBTree = new BTree<number>();
+    // newBTree.insert(20);
+    // newBTree.insert(40);
+    // newBTree.insert(10);
+    // newBTree.insert(30);
+    // newBTree.insert(50);
+    // newBTree.insert(11);
+    // newBTree.insert(12);
+    // newBTree.insert(13);
+    // newBTree.delete(50);
+    // newBTree.delete(40);
+
+    const newBTree = new BTree<number>();
+
+    // Insert keys to create the required structure
+    newBTree.insert(10);
+    newBTree.insert(20);
+    newBTree.insert(5);
+    newBTree.insert(6);
+    newBTree.insert(15);
+    newBTree.insert(30);
+    newBTree.insert(25);
+    newBTree.insert(16);
+    newBTree.delete(30);
+    
+    // Set the new BTree instance to state
+    setBtree(newBTree);
+  }, []);
 
 
   const returnNodes = (node: BTreeNode<number>): JSX.Element => {
@@ -32,16 +64,31 @@ function App() {
   };
 
   const insertNewValue = () => {
+    findMethod('insert');
+  };
+
+  const deleteValue = () => {
+    findMethod('delete');
+  };
+
+  const findMethod = (method: 'insert' | 'delete') => {
     const numValue = Number(value);
     if (!isNaN(numValue)) {
-      btree.insert(Number(value));
-      setBtree(btree)
+      switch (method) {
+        case 'insert':
+          btree.insert(Number(value));
+          setBtree(btree)
+          break;
+        case 'delete':
+          btree.delete(Number(value));
+          setBtree(btree)
+          break;
+      }
       setValue('');
     } else {
       alert('Please insert number!')
     }
-  };
-
+  }
   const onChangeValue = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e) setValue(e.target.value);
   };
@@ -50,6 +97,7 @@ function App() {
       <div className='w-full flex  gap-[1em] justify-center mb-[0.5em]'>
         <input className='bg-grey  w-[10%] border' value={value} type='text' placeholder='Add number...' onChange={e => onChangeValue(e)}></input>
         <button className='bg-[#808080] px-[0.5em]' onClick={() => insertNewValue()}>Insert</button>
+        <button className='bg-[#808080] px-[0.5em]' onClick={() => deleteValue()}>Delete</button>
       </div>
 
       {returnNodes(btree.returnRoot())}
--- src/B-Tree-Logic/B-Tree-Node.ts
+++ src/B-Tree-Logic/B-Tree-Node.ts
@@ -12,43 +12,123 @@ export class BTreeNode<T> {
 export class BTree<T> {
     private root: BTreeNode<T>;
     private degree = 2;
+    private parentNodeForDeletionUse: BTreeNode<T>;
 
     constructor() {
         this.root = new BTreeNode<T>(true);
+        this.parentNodeForDeletionUse = new BTreeNode<T>(false);
     };
 
     public insert(key: T) {
 
-        if (this.root.keys.length === 2 * this.degree - 1) {
+        this.inserKey(this.root, key);
+        if (this.root.keys.length === this.degree + 1) {
             const newRoot = new BTreeNode<T>(false);
             newRoot.children.push(this.root);
             this.splittingNode(newRoot, 0, this.root);
             this.root = newRoot;
+        };
 
-            this.inserKey(newRoot, key);
-        } else {
-            this.inserKey(this.root, key);
-        }
     };
-    public returnRoot() {
-        return this.root;
-    }
+
     private inserKey(node: BTreeNode<T>, key: T) {
+
         let i = node.keys.length - 1;
         while (i >= 0 && node.keys[i] > key) i--;
         if (node.isLeaf) {
             node.keys.splice(i + 1, 0, key);
         } else {
             i++
-            if (node.children[i].keys.length === 2 * this.degree - 1) {
+
+            this.inserKey(node.children[i], key);
+            if (node.children[i].keys.length === this.degree + 1) {
                 this.splittingNode(node, i, node.children[i]);
                 if (key > node.keys[i]) {
                     i++;
                 };
             };
-            this.inserKey(node.children[i], key);
+
+
         }
     };
+    public returnRoot() {
+        return this.root;
+    };
+    public delete(key: T) {
+        const node = this.search(key);
+        if (node && node.isLeaf) this.deleteKeyFromLeaf(node, key);
+    };
+    private deleteKeyFromLeaf(node: BTreeNode<T>, key: T) {
+        const keyIndex = node.keys.findIndex(v => v === key);
+
+        if (keyIndex === -1) return;
+
+
+        if (node.keys.length > 1) {
+            node.keys.splice(keyIndex, 1);
+            return;
+        }
+
+        let i = this.parentNodeForDeletionUse.keys.length;
+        while (i > 0 && this.parentNodeForDeletionUse.keys[i] > key) i--;
+        const childIndex = this.parentNodeForDeletionUse.children.findIndex(n => n === node);
+        const leftSiblingNode = this.parentNodeForDeletionUse.children[childIndex - 1];
+        // const leftSiblingNode = this.parentNodeForDeletionUse.children[childIndex - 1] ? this.parentNodeForDeletionUse.children[childIndex - 1] : null;
+
+        const rightSiblingNode = this.parentNodeForDeletionUse.children[childIndex + 1];
+        if (leftSiblingNode) {
+            if (leftSiblingNode.keys.length > 1 && this.parentNodeForDeletionUse.keys.length > 1) {
+                const b = this.parentNodeForDeletionUse.keys[0] > leftSiblingNode.keys[i - 1] ? 0 : 1;
+
+                node.keys.splice(keyIndex, 1, this.parentNodeForDeletionUse.keys[b]);
+                this.parentNodeForDeletionUse.keys.splice(b, 1, leftSiblingNode.keys[i - 1])
+                leftSiblingNode.keys.length--;
+
+            } else if (this.parentNodeForDeletionUse.keys.length > 1 && leftSiblingNode.keys.length === 1) {
+                const b = this.parentNodeForDeletionUse.keys[1] > key ? 0 : 1;
+                leftSiblingNode.keys.push(this.parentNodeForDeletionUse.keys[b])
+                this.parentNodeForDeletionUse.children.splice(childIndex, 1);
+                this.parentNodeForDeletionUse.keys.splice(b, 1);
+                // this.parentNodeForDeletionUse.keys.length--;
+            } else if (this.parentNodeForDeletionUse.keys.length === 1 && leftSiblingNode.keys.length > 1) {
+                this.parentNodeForDeletionUse.children[i].keys.splice(0, 1, this.parentNodeForDeletionUse.keys[0]);
+                this.parentNodeForDeletionUse.keys.splice(0, 1, leftSiblingNode.keys[i])
+                leftSiblingNode.keys.length--;
+            } else if (this.parentNodeForDeletionUse.keys.length === 1 && leftSiblingNode.keys.length === 1) {
+                leftSiblingNode.keys.push(this.parentNodeForDeletionUse.keys[0]);
+                this.handleDeleteRootWithSingleKeyAndSingleLeafsKey(this.parentNodeForDeletionUse, leftSiblingNode);
+            }
+        } else if (rightSiblingNode) {
+            if (this.parentNodeForDeletionUse.keys.length === 1 && rightSiblingNode.keys.length === 1) {
+                rightSiblingNode.keys.splice(0, 0, this.parentNodeForDeletionUse.keys[0]);
+
+                this.handleDeleteRootWithSingleKeyAndSingleLeafsKey(this.parentNodeForDeletionUse, rightSiblingNode);
+            };
+
+        };
+
+    };
+    private handleDeleteRootWithSingleKeyAndSingleLeafsKey(parentNodeForDeletionUse: BTreeNode<T>, siblingNode: BTreeNode<T>) {
+        this.parentNodeForDeletionUse.keys = [];
+        this.parentNodeForDeletionUse.children = [];
+        if (this.root === this.parentNodeForDeletionUse) {
+            this.root = siblingNode;
+        };
+    }
+    public search(key: T): BTreeNode<T> | null {
+        return this.searchInNode(this.root, key);
+    };
+    private searchInNode(node: BTreeNode<T>, key: T): BTreeNode<T> | null {
+        let i = 0;
+        while (i < node.keys.length && node.keys[i] < key) i++;
+        if (node.keys[i] === key) return node;
+
+        if (node.isLeaf) return null;
+        this.parentNodeForDeletionUse = node;
+        return this.searchInNode(node.children[i], key);
+    };
+
+
 
     private splittingNode(newParent: BTreeNode<T>, index: number, oldRoot: BTreeNode<T>) {
         const newChildNode = new BTreeNode<T>(oldRoot.isLeaf);
